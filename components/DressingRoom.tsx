dc9603bc6733ba8180e6c0945cffb79a9a78d08d
letscodesatish <satish.kum.14.sk@gmail.com>
2026-08-27 14:59:00 +0530

    scaffold: add Dressing Room page shell

---
+++ components/DressingRoom.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Navbar from "./Navbar";
+import JerseyCard from "./JerseyCard";
+import Scorecard from "./Scorecard";
+import Timeline from "./Timeline";
+import Certificates from "./Certificates";
+import ContactForm from "./ContactForm";
+import { profile } from "@/data/profile";
+import { Project, Skill, Certificate as CertificateType } from "@/lib/types";
+
+
+export default function DressingRoom() {
+  // TODO: implement
+  return null;
+}

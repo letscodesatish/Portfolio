bc4ef107752f5683b5e6b1ec8b1e83cfcb4532b9
letscodesatish <satish.kum.14.sk@gmail.com>
2026-08-27 15:23:00 +0530

    scaffold: add navbar shell

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import { Menu, Volume2, VolumeX, X } from "lucide-react";
+import { profile } from "@/data/profile";
+import ProfileModal from "./ProfileModal";
+import { useSound } from "./providers/SoundProvider";
+
+
+export default function Navbar() {
+  // TODO: implement
+  return null;
+}

81583ebe5b4a014bfb748f8e77c0539a188dd797
letscodesatish <satish.kum.14.sk@gmail.com>
2026-08-28 13:12:00 +0530

    scaffold: add Career Innings timeline shell

---
+++ components/Timeline.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { MapPin, Sparkles } from "lucide-react";
+import { education } from "@/data/education";
+import { EducationChapter, EducationStage } from "@/lib/types";
+import { SectionHeading } from "./Scorecard";
+
+
+export default function Timeline() {
+  // TODO: implement
+  return null;
+}

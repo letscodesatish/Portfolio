4c9d9b0dc3d3d2c7b7e0b04852ee42f816df7faa
letscodesatish <satish.kum.14.sk@gmail.com>
2026-08-28 11:50:00 +0530

    scaffold: add jersey-styled project card shell

---
+++ components/JerseyCard.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Link from "next/link";
+import { useRef, useState } from "react";
+import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
+import { Code2, ExternalLink } from "lucide-react";
+import { Project } from "@/lib/types";
+import { useSound } from "./providers/SoundProvider";
+
+
+export default function JerseyCard() {
+  // TODO: implement
+  return null;
+}

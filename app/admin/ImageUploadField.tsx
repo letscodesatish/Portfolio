a605556b7a311dbf61878f2afcc42037cc96bd69
letscodesatish <satish.kum.14.sk@gmail.com>
2026-08-28 16:36:00 +0530

    scaffold: add admin image upload field shell

---
+++ app/admin/ImageUploadField.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useRef, useState } from "react";
+import { ImageUp, Loader2, X } from "lucide-react";
+import { uploadAsset } from "./actions";
+
+
+export default function ImageUploadField() {
+  // TODO: implement
+  return null;
+}

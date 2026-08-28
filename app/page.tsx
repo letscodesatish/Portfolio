ccb523d0d74e9c2a37abc709f8f00f6cdd679d39
letscodesatish <satish.kum.14.sk@gmail.com>
2026-08-28 17:55:00 +0530

    scaffold: add home page shell

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import HomeClient from "@/components/HomeClient";
+import { getProjects } from "@/lib/data/projects";
+import { getSkills } from "@/lib/data/skills";
+import { getCertificates } from "@/lib/data/certificates";
+
+
+export default async function Home() {
+  // TODO: implement
+  return null;
+}
